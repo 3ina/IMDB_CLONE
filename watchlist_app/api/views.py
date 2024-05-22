7a5df7f3b3bfda269bd46cd162c82a9936758688
sina <3inroydl@gmail.com>
2024-05-22 19:56:34 +0330

    add movie detail view

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from watchlist_app.models import Movie
@@ -19,5 +20,39 @@ class MovieList(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+
+class MovieDetail(APIView):
+
+    def get(self,request:HttpRequest,pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error':'movie not found'},status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self,request : HttpRequest,pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error':'movie not found'},status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie,request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request:HttpRequest,pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'movie not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
