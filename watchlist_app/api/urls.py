e13334f23a71d0035fc73a1e404caf3ecb18ebc4
sina <3inaroydl@gmail.com>
2024-05-26 12:55:20 +0330

    add reviewDetail View

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from watchlist_app.api.views import \
-    WatchList , WatchDetail ,StreamPlatformList ,\
-    StreamPlatformDetail , ReviewList
+    WatchList, WatchDetail, StreamPlatformList, \
+    StreamPlatformDetail, ReviewList, ReviewDetail
 
 urlpatterns = [
-    path('WatchList/',WatchList.as_view(),name='movie-list'),
-    path('WatchList/<int:pk>',WatchDetail.as_view(),name='movie-detail'),
-    path('stream/',StreamPlatformList.as_view(),name='stream-list'),
+    path('WatchList/', WatchList.as_view(), name='movie-list'),
+    path('WatchList/<int:pk>', WatchDetail.as_view(), name='movie-detail'),
+    path('stream/', StreamPlatformList.as_view(), name='stream-list'),
     path('stream/<int:pk>', StreamPlatformDetail.as_view(), name='stream-detail'),
-    path('review/',ReviewList.as_view(),name='review-list')
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 
 ]
