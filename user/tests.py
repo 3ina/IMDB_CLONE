d2e1c1ad87c10dc68c79df15d25e871c68c94d5c
sina <3inaroydl@gmail.com>
2024-06-15 20:31:53 +0330

    add register test

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+
+class RegisterTestCase(APITestCase):
+    def test_register(self):
+        data = {
+            "username" : "testcase",
+            "email" : "testcase@gmail.com",
+            "password" : "password",
+            "password2": "password",
+
+        }
+
+        response = self.client.post(reverse('register'),data)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+        data = {
+            "username": "testcase",
+            "email": "testcase@gmail.com",
+            "password": "password",
+
+
+        }
+
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
